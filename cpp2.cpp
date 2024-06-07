bc43d146b10da91726344bb4f1eecf00774fa5ab
SoulOfHope <97285437+SoulOfHope@users.noreply.github.com>
2024-06-07 11:19:51 +0000

    Another one

---
+++ cpp2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string name;
+    std::cout << "Hello World" << std::endl;
+    std::cout << "What is your name? ";
+    std::cin >> name;
+    std::cout << "Hello " << name << std::endl;
+    return 0;
+}

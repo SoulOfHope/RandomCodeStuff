10731a24cfa9088f9da87260e6286f469c082615
SoulOfHope <97285437+SoulOfHope@users.noreply.github.com>
2024-06-11 12:48:26 +0000

    Writing stuff

--- cpp2.cpp
+++ cpp2.cpp
@@ -1,12 +1,22 @@
+#include <cstdlib>
+#include <fstream>
 #include <iostream>
 #include <string>
 using namespace std;
 
 int main() {
     string name;
-    std::cout << "Hello World" << std::endl;
+    int age;
+    bool saved;
     std::cout << "What is your name? ";
     std::cin >> name;
-    std::cout << "Hello " << name << std::endl;
+    std::cout << "Hello " << name << "." << std::endl;
+    std::cout << "Enter your age: ";
+    std::cin >> age;
+    if (age<12) {
+        std::cout << "I'm afraid you're too young for this adventure. Try again later in life." << std::endl;
+        std::exit(0);
+    };
+    std::cout << "Excellent. Lets begin." << std::endl;
     return 0;
 }

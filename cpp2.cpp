dca33562dc836b50f815015bd55f899d6566d45a
SoulOfHope <97285437+SoulOfHope@users.noreply.github.com>
2024-06-12 10:28:21 +0000

    Settings and stuff

--- cpp2.cpp
+++ cpp2.cpp
@@ -1,17 +1,45 @@
+// Imports
+#include <cstdlib>
 #include <iostream>
 #include <fstream>
+#include <filesystem>
 #include <string>
+#include <vector>
+
+// Namespace Declarations
+namespace fs = std::filesystem;
+
+//Declare Using Namespace STD
+using namespace std;
+
+std::vector<std::byte> readFileData(const std::string& name) {
+    std::filesystem::path inputFilePath{name};
+    auto length = std::filesystem::file_size(inputFilePath);
+    if (length == 0) {
+        return {};  // empty vector
+    }
+    std::vector<std::byte> buffer(length);
+    std::ifstream inputFile(name, std::ios_base::binary);
+    inputFile.read(reinterpret_cast<char*>(buffer.data()), length);
+    inputFile.close();
+    return buffer;
+}
 
 int main() {
     std::string name;
-    int age;
+    int age = 0;
     bool saved;
 
-    std::cout << "What is your name? ";
-    std::cin >> name;
-    std::cout << "Hello " << name << "." << std::endl;
-    std::cout << "Enter your age: ";
-    std::cin >> age;
+    if (fs::exists("save.bin") && fs::is_empty("save.bin")!=true) {
+        std::vector data = readFileData("save.bin");
+    }
+    else {
+        std::cout << "What is your name? ";
+        std::cin >> name;
+        std::cout << "Hello " << name << "." << std::endl;
+        std::cout << "Enter your age: ";
+        std::cin >> age;
+    }
 
     if (age < 12) {
         std::cout << "I'm afraid you're too young for this adventure. Try again later in life." << std::endl;
@@ -28,6 +56,7 @@ int main() {
         outfile.write(reinterpret_cast<const char*>(&age), sizeof(age));
         outfile.close();
         std::cout << "User information saved successfully." << std::endl;
+        saved = true;
     } else {
         std::cout << "Failed to open file for writing." << std::endl;
     }
